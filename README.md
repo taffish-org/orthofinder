a24f33ca81b22ed9bf07e46f458c5d3f21301e0b
Kaiyuan Han <hermit_han@163.com>
2026-02-09 15:23:27 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# taf-orthofinder
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/OrthoFinder/OrthoFinder
+
+Phylogenetic orthology inference for comparative genomics
---
+++ orthofinder.info.lisp
@@ -0,0 +1,20 @@
+(:name           "orthofinder")
+(:repo           "taffish-org/orthofinder")
+(:author         "KaiyuanHan")
+(:version        "3.1.2")
+(:intro          "Phylogenetic orthology inference for comparative genomics")
+(:dependencies)
+(:functions      "orthofinder"
+                 "python3"
+                 "mcl      [22-282]"
+                 "blast    [2.17.0+]"
+                 "diamond  [2.1.22]"
+                 "fastme   [2.1.6.4]"
+                 "mmseqs   [2-17-b804f]"
+                 "famsa    [2.4.1]"
+                 "mafft    [7.525]"
+                 "muscle   [5.3]"
+                 "FastTree [2.2.0]"
+                 "raxmlHPC [8.2.12]"
+                 "raxml-ng [1.2.2]"
+                 "iqtree3  [3.0.1]")
